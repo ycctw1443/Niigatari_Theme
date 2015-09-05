2fc0ef76a1006c62430c434b4dff3396994a32f3
ycctw1443 <ycctw1443@gmai.com>
2015-09-06 03:22:12 +0900

    ウィジェット2を追加

--- functions.php
+++ functions.php
@@ -4,6 +4,16 @@ add_theme_support('menus');
 
 register_sidebar(
     array(
+        'name' => 'ウィジェット1'
+        'id' => '1'
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_titile' => '</h3>',
+    )
+    array(
+        'name' => 'ウィジェット2'
+        'id' => '2'
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
