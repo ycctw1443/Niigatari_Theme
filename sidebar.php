35a7151ec05ee0094a55954b12bbbf97d480b9b3
ycctw1443 <ycctw1443@gmai.com>
2015-09-08 21:52:41 +0900

    サイドバーの最新記事部分の修正

--- sidebar.php
+++ sidebar.php
@@ -3,31 +3,28 @@
     <?php endif; ?>
 
     <div class="sidebar-posts">
-
+	最近の記事<br>
     <?php
-        if (have_posts()) :
-            while (have_posts()) :
-                    the_post();
+    	if (have_posts()) :
+        	while (have_posts()) :
+                	the_post();
     ?>
 
-    <div class="sidebar-post">
-            <?php if (has_post_thumbnail()) : ?>
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150,150)); ?></a>
-            <br>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                <?php else: ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/niigatari_thumbnail.jpg" width="100" height="100">
-                <?php endif; ?>
+	<div class="sidebar-post">
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150,150)); ?></a>
+		<br>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
     <?php
-            endwhile;
+        	endwhile;
         else:
     ?>
-        <p>記事はありません</p>
+    	<p>記事はありません</p>
     <?php
-            endif;
+    		endif;
     ?>
+	</div>
     </div>
-    </div>
+
 
 </div>
