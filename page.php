66f0c23c76c31f87975a7fe38398d37631eb18da
ycctw1443 <ycctw1443@gmai.com>
2015-09-08 22:46:16 +0900

    ポスト表示の修正

--- page.php
+++ page.php
@@ -1,6 +1,40 @@
 <?php get_header(); ?>
         <div class="main">
             <div class="contents">
+                <div class="posts">
+
+                   <?php
+                   if (have_posts()) :
+                       while (have_posts()) :
+                           the_post();
+                   ?>
+
+                   <div class="post">
+                       <div class="post-header">
+                           <h2>
+                               <?php the_title(); ?>
+                           </h2>
+
+                       </div>
+                       <div class="post-content">
+
+                           <div class="post-body">
+                               <?php the_content(); ?>
+                           </div>
+                       </div>
+                   </div>
+
+
+                   <?php
+                       endwhile;
+                   else:
+                   ?>
+                   <p>ページはありません</p>
+                   <?php
+                       endif;
+                   ?>
+
+               </div>
 
             </div>
 
