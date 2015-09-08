06940c6a47fd099c88a02744ee292b8b7aa0af7e
ycctw1443 <ycctw1443@gmai.com>
2015-09-08 21:39:15 +0900

    サイドバーに投稿記事を表示刷る機能追加

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,33 @@
 <div class="sidebar">
     <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(2) ) : else : ?>
     <?php endif; ?>
+
+    <div class="sidebar-posts">
+
+    <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                    the_post();
+    ?>
+
+    <div class="sidebar-post">
+            <?php if (has_post_thumbnail()) : ?>
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150,150)); ?></a>
+            <br>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <?php else: ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/niigatari_thumbnail.jpg" width="100" height="100">
+                <?php endif; ?>
+
+    <?php
+            endwhile;
+        else:
+    ?>
+        <p>記事はありません</p>
+    <?php
+            endif;
+    ?>
+    </div>
+    </div>
+
 </div>
